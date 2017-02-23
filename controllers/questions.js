ab758d70ee225f5f16a47fe295b902e692b26b86
James Annal <jamesannal@gmail.com>
2017-02-23 16:29:38 +0000

    trying to merge again

---
+++ controllers/questions.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var questionRouter = express.Router();
+
+var QuestionsQuery = require('../client/db/questionsQuery');
+var query = new QuestionsQuery();
+
+questionRouter.get('/', function(req, res) {
+  query.all(function(res) {
+    res.json(res);
+  });
+});
+
+modules.exports = questionRouter;
