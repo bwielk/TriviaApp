b05200be073d11d8e568b87b8e642be66ec120c8
James Annal <jamesannal@gmail.com>
2017-02-23 15:40:25 +0000

    Added seeds

---
+++ seeds.js
@@ -0,0 +1,17 @@
+use triva;
+
+db.questions.insert([
+  
+  {
+    question: "What is 1 + 1?",
+    options: ["1", "3", "11", "2"],
+    correctAnswer: "2"
+  }
+
+  {
+    question: "What show made the Olsen twins famous?"
+    options: ["Family Matters","Full House","Friends","Blossom"],
+    correctAnswer: "Full House"
+  }
+
+]);
