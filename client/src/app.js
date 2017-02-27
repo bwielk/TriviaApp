c4e68aaad94e2012b93960237b533d2e89b3c126
Nina Kahr <ntkahr@gmail.com>
2017-02-27 13:40:23 +0000

    remove leaderboard route

--- client/src/app.js
+++ client/src/app.js
@@ -1,17 +1,7 @@
-var gameUI = require('./views/gameUI.js');
 var welcomeUI = require('./views/welcomeUI.js');
-var registrationUI = require('./views/registrationUI.js');
 
 var app = function() {
-  new gameUI();
-}
-
-var welcome = function() {
   new welcomeUI();
 }
 
-var registration = function() {
-  new registrationUI();
-}
-
-window.onload = welcome;
+window.onload = app;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,14 +1,27 @@
 config = {
-   entry: {
-      welcome: "./src/js/app.js",
-      leaderboard: "./src/js/leaderboardApp.js"
-    },
-    output: {
-      path: "./build",
-      filename: "[name]-bundle.js"
-    },
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: "./build"
+  },
+  devtool: 'source-map' 
+};
 
-    devtool: "source-map"
-  };
+
+
+
+
+// config = {
+//    entry: {
+//       welcome: "./src/js/app.js",
+//       leaderboard: "./src/js/leaderboardApp.js"
+//     },
+//     output: {
+//       path: "./build",
+//       filename: "[name]-bundle.js"
+//     },
+
+//     devtool: "source-map"
+//   };
 
 module.exports = config;
